9b83b586f6641155aad86f3a8a63515df60b09e7
ketu.lai <ketu.lai@gmail.com>
2017-06-27 09:49:06 +0800

    update

---
+++ src/Veda/Utils/Converter/XmlToArray.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author [ketu.lai]
+ * @email [ketu.lai@gmail.com]
+ * @create date 2017-03-29 04:59:13
+ * @modify date 2017-03-29 04:59:13
+ * @desc [description]
+*/
+
+namespace Veda\Utils\Converter;
+
+use DOMElement;
+use DOMDocument;
+
+class XmlToArray 
+{
+    public static function convert($xml)
+    {
+
+    }
+
+    public static function fromFile($filename)
+    {
+        
+    }
+}
